d4261ee4fe1bd02478b02d45bb874c0b56813d3c
Porphyry4x4 <100414984+Porphyry4x4@users.noreply.github.com>
2022-05-08 16:44:56 +0100

    several bugs are fixed eg. props nor bieng passed correctly

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,9 +12,32 @@ class App extends React.Component {
     this.state = {
       searchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: '1' },
       { name: 'name2', artist: 'artist2', album: 'album2', id: '2' },
-      { name: 'name3', artist: 'artist3', album: 'album3', id: '3' }]
+      { name: 'name3', artist: 'artist3', album: 'album3', id: '3' }],
+      playListName: 'Playlist',
+      playListTrack: [{ name: 'playlistname1', artist: 'playlistartist1', album: 'play;istalbum1', id: '4' },
+      { name: 'playlistname2', artist: 'playlistartist2', album: 'play;istalbum2', id: '5' },
+      { name: 'playlistname3', artist: 'playlistartist3', album: 'play;istalbum3', id: '6' }]
     }
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
+
+  addTrack(track) {
+    let tracks = this.state.playListTrack;
+    if (tracks) {
+    if (tracks.find(savedTrack => track.id === savedTrack.id)) {
+    return;    
+  } else {
+    tracks.push(track);
+    this.setState({ playListTrack: tracks})};
+  }
+}
+removeTrack(track) {
+  let tracks = this.state.playListTrack;
+  tracks = tracks && tracks.filter(currentTrack => currentTrack.id !== track.id);
+  this.setState({ playListTrack: tracks})
+}
+
   render() {
     return (
     <div>
@@ -22,8 +45,11 @@ class App extends React.Component {
   <div className="App">
     <SearchBar />
     <div className="App-playlist">
-      <Playlist />
-      <SearchResults searchResults={ this.state.searchResults }/>
+      <Playlist playListName={ this.props.playListName }
+                playListTrack={ this.state.playListTrack }
+                onRemove={ this.removeTrack }/>
+      <SearchResults searchResults={ this.state.searchResults }
+                    onAdd={ this.addTrack}/>
     </div>
   </div>
 </div>)
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -9,7 +9,9 @@ class Playlist extends React.Component {
         return (
             <div className="Playlist">
               <input defaultValue={ 'New Playlist' }/>
-              <Tracklist />
+              <Tracklist tracks={ this.props.playListTrack }
+                         onRemove={ this.props.onRemove }
+                         isRemoval={true} />
    
             <button className="Playlist-save">SAVE TO SPOTIFY</button>
 </div>
